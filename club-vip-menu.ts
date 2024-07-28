7883670a4eebfbdbb3c5bacf892b07cb44267ee7
Daniel Emenike <emenike.daniel@palmpay-inc.com>
2024-07-28 05:12:14 +0100

    chivas updated

--- club-vip-menu.ts
+++ club-vip-menu.ts
@@ -30,7 +30,7 @@ export const vipMenu = [
           { id: 7, name: "REMY MARTINS XO", price: "N450,000", image: "/rmxo.jpeg" },
           { id: 8, name: "REMY MARTINS VSOP", price: "N100,000", image: "/rmvs.png" },
           { id: 9, name: "REMY MARTINS 1738", price: "N150,000", image: "/rmmartins.jpeg" },
-          { id: 10, name: "CHIVAS 25 YEARS", price: "N250,000", image: "/chivas25years.webp" },
+          
         
         ]
     },
@@ -54,6 +54,7 @@ export const vipMenu = [
             { id: 14, name: "JAMESON GREEN BARREL", price: "N40,000", image: "/gb.png" }, 
             { id: 15, name: "JAMESON BLACK BARREL", price: "N50,000", image: "/jbb.jpeg" },
           { id: 16, name: "MONKEY SHOULDER", price: "N40,000", image: "/mshh.jpeg" },
+          { id: 21, name: "CHIVAS 25 YEARS", price: "N250,000", image: "/chivas25years.webp" },
           { id: 18, name: "CHIVAS REGAL 18 YEARS", price: "N120,000", image: "/chivas18.jpg" },
           { id: 19, name: "CHIVAS XV 15 YEARS", price: "N90,000", image: "/chivasxv15.jpeg" },
         ]
