babcb604e715922a40d91b3801d615c2f8a94325
carabanaofficial <Carabanaofficial@gmail.com>
2024-09-25 23:22:35 +0100

    Update lounge-regular-menu.ts

--- lounge-regular-menu.ts
+++ lounge-regular-menu.ts
@@ -57,7 +57,6 @@ export const regularMenu = [
           { id: 18, name: "CHIVAS REGAL 18 YEARS", price: "N120,000", image: "/chivas18.jpg" },
           { id: 19, name: "CHIVAS XV 15 YEARS", price: "N90,000", image: "/chivasxv15.jpeg" },
             { id: 10, name: "CHIVAS 25 YEARS", price: "N250,000", image: "/chivas25years.webp" },
-            { id: 20, name: "OBSERVATORY 20YRS", price: "N150,000", image: "/observatory-20y.jpg" },
               { id: 20, name: "OBSERVATORY 20YRS", price: "N150,000", image: "/observatory-20y.jpg" },
             { id: 21, name: "BALVENIE", price: "120,000", image: "/balvenie.jpeg" },
             { id: 22, name: "GENTLEMAN JACK", price: "100,000", image: "/GentlemanJack.webp" },
