72a6b9acdec8cf9a38f7e74f0728c2b18c4089b9
carabanaofficial <Carabanaofficial@gmail.com>
2024-09-26 02:14:08 +0100

    Update sky-menu.ts

--- sky-menu.ts
+++ sky-menu.ts
@@ -80,6 +80,10 @@ export const skyMenu = [
         { id: 12, name: "CRONIER WINE", price: "N15,000", image: "/cron.webp" },
         { id: 13, name: "CHAMDOR", price: "N10,000", image: "/chamdor.jpeg" },
         { id: 14, name: "VELETA WINE", price: "N10,000", image: "/veleta_wine.webp" },
+         { id: 15, name: "BELENA MOSCATO", price: "50,000", image: "/belena moscato.jpeg" },
+              { id: 16, name: "BELENA SHIRAZ", price: "50,000", image: "/belenashiraz.png" },
+               { id: 17, name: "VELETA WINE", price: "10,000", image: "/veleta wine.webp" },
+                { id: 18, name: "MIRACLE WINE", price: "15,000", image: "/Miraclewine.jpg" } 
       ]
     },
   
