56f657290983bb6f301a3028578a7f4f05df52d4
carabanaofficial <Carabanaofficial@gmail.com>
2024-09-26 01:54:17 +0100

    Update club-vip-menu.ts

--- club-vip-menu.ts
+++ club-vip-menu.ts
@@ -83,7 +83,7 @@ export const vipMenu = [
         { id: 12, name: "CRONIER WINE", price: "N15,000", image: "/cron.webp" },
         { id: 14, name: "GUCCI WINE", price: "N15,000", image: "/gucciwine.jpg" },
           { id: 15, name: "BELENA MOSCATO", price: "50,000", image: "/belena moscato.jpeg" },
-              { id: 16, name: "BELENA SHIRAZ", price: "50,000", image: 
+              { id: 16, name: "BELENA SHIRAZ", price: "50,000", image: "/belenashiraz.png" },
                { id: 17, name: "VELETA WINE", price: "10,000", image:
                 { id: 18, name: "MIRACLE WINE", price: "15,000", image:
         
