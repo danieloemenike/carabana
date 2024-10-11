9e5cdb5500e8936256bfc4fce8ed82cc6a47861e
carabanaofficial <Carabanaofficial@gmail.com>
2024-10-11 23:48:47 +0100

    Update lounge-regular-menu.ts

--- lounge-regular-menu.ts
+++ lounge-regular-menu.ts
@@ -124,7 +124,7 @@ export const regularMenu = [
         id: 6,
         category: "Soft Drink / Mixers",
         drinks: [
-            { id: 1, name: "COKE", price: "N500", image: "/coke.png" },
+            { id: 1, name: "COKE", price: "N1,000", image: "/coke.png" },
             { id: 2, name: "SPRITE", price: "N500", image: "/sprite.jpeg" },
             { id: 3, name: "SCHWEPPES", price: "N500", image: "/schw.avif" },
             { id: 4, name: "FAYROUZ", price: "N700", image: "/fayrouz-pet-x12.jpg" },
